bd00eb60ca34d849b7d605ec3026391ece3f280f
Andrew Mukiza <amukiza@otunda.com>
2018-10-17 22:11:08 -0400

    Uses 4 space indentantion

--- src/model/order.py
+++ src/model/order.py
@@ -1,6 +1,8 @@
 import src.model.customer as Customer
 from src.model.shoppingcart import ShoppingCart
+
+
 class Order(ShoppingCart):
- def __init__(self, customer = Customer, products = []):
-  super().__init__(customer, products, "ORDER_PLACED")
-  return
+    def __init__(self, customer=Customer, products=[]):
+        super.__init__(customer, products, "ORDER_PLACED")
+        return
